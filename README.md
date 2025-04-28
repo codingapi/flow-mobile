bbd9b53f2ce25989bd5c810eaacf7f12bc284d5a
lorne <1991wangliang@gmail.com>
2025-04-28 11:43:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# @codingapi/flow-mobile
+
+一个基于 React 和 TypeScript 的流程设计器组件库。
---
+++ rollup.config.js
@@ -0,0 +1,64 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
+import del from 'rollup-plugin-delete';
+
+export default {
+    input: 'src/index.ts',
+    external: [
+        'react',
+        'react-dom',
+        'antd',
+        '@codingapi/ui-framework',
+        '@logicflow/core',
+        '@logicflow/extension',
+        '@reduxjs/toolkit',
+        'react-redux'
+    ],
+    output: [
+        {
+            dir: 'dist',
+            format: 'cjs',
+            sourcemap: true,
+            entryFileNames: 'index.js',
+            exports: 'named',
+            preserveModules: false,
+        },
+        {
+            dir: 'dist',
+            format: 'esm',
+            sourcemap: true,
+            entryFileNames: 'index.es.js',
+            exports: 'named',
+            preserveModules: false,
+        },
+    ],
+    plugins: [
+        del({targets: 'dist/*'}),
+        peerDepsExternal(),
+        resolve({
+            preferBuiltins: true,
+            extensions: ['.ts', '.tsx', '.js', '.jsx']
+        }),
+        commonjs(),
+        typescript({
+            tsconfig: 'tsconfig.json',
+            sourceMap: true,
+            declaration: true,
+            declarationDir: 'dist'
+        }),
+        postcss({
+            extract: false,
+            modules: {
+                generateScopedName: '[name]__[local]___[hash:base64:5]',
+            },
+            use: ['sass'],
+            minimize: true,
+            inject: {
+                insertAt: 'top',
+            },
+        }),
+    ],
+};
